e4560e9b4020e651ff56e51c0bcc7f460271a899
Dhruvit Salat <salatd0852@gmail.com>
2023-04-06 23:59:49 +0530

    Initial commit

---
+++ public/js/script.js
@@ -0,0 +1,42 @@
+function toggleOnShow(){
+    let dates = document.getElementById("dates");    
+    let x = document.getElementById("form"); 
+    if(x.style.display === 'none'){
+        x.style.display='block';
+    }   
+    else{
+        x.style.display='none';
+    }
+}
+function toggleShow(){
+    let room = document.getElementById("room");    
+    let y = document.getElementById("alll"); 
+    if(y.style.display === "none"){
+        y.style.display='block';
+    }   
+    else{
+        y.style.display='none';
+    }
+}
+function toggleShoww(){
+    let guest = document.getElementById("guest");    
+    let z = document.getElementById("grid"); 
+    if(z.style.display === "none"){
+        z.style.display='block';
+    }   
+    else{
+        z.style.display='none';
+    }
+}
+function toggleAtShoww(){
+    let final = document.getElementById("final");    
+    let a = document.getElementById("atlast"); 
+    if(a.style.display === "none"){
+        a.style.display='block';
+    }   
+    else{
+        a.style.display='none';
+    }
+}
+
+  
